02ebb8e2cb9a15c426e46f897fcf79b3d6422824
Lenayah <lfanesie@gmail.com>
2024-05-06 12:13:02 -0400

    update

---
+++ NamestoFaces/Person.swift
@@ -0,0 +1,19 @@
+//
+//  Pearson.swift
+//  NamestoFaces
+//
+//  Created by 11k on 5/6/24.
+//
+
+import UIKit
+
+class Person: NSObject {
+    var name: String
+    var image: String
+    
+    init(name: String, image: String) {
+        self.name = name
+        self.image = image
+    }
+
+}
